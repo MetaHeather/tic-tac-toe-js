09181f19190ec34e944f08ae942f07e1dd9f00d7
Heather Etheridge <heathernetheridge@gmail.com>
2019-07-27 13:58:20 -0500

    board syle layed out in css and represented in js as a nested array

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*----- constants -----*/ 
+
+
+/*----- app's state (variables) -----*/ 
+let board, turn, winner;
+
+
+/*----- cached element references -----*/ 
+
+
+/*----- event listeners -----*/ 
+function init () {
+  board = [
+      [0,0,0],
+      [0,0,0],
+      [0,0,0]
+  ];
+  turn = 1;
+  winner = null;
+  render();
+}
+
+/*----- functions -----*/
+function render () {
+    
+}
