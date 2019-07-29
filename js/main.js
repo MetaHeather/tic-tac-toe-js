66aa25e9b6823c0fc1872b2c560853922bf2ccc9
Heather Etheridge <heathernetheridge@gmail.com>
2019-07-28 20:43:24 -0500

    Added render() for printing x/o to clicked div

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 /*----- constants -----*/ 
-
+const SYMBOL = {
+  '0': '',
+  '1': '&#120299;',
+  '-1': '&#120290;'
+}
 
 /*----- app's state (variables) -----*/ 
 
@@ -28,8 +32,24 @@ function init () {
   }
   
 function render () {
-  
+  //stores location of msg tag  
+  let msg = document.getElementById('msg');
+  //Changes message based on whose turn it is
+  msg.textContent = (player === 1) ? "X's Turn" : "O's Turn";
+  //Update div content to reflect info inside board object.
+  //Loop through each row in the board and do function
+  board.forEach(function(rowArray, rowIdx) {
+    //Loop through each column in that row
+    rowArray.forEach(function (colVal, colIdx) {
+      //Looping through board and selecting each corresponding html div with given data values
+      //Uses an attribute selector to use the div with the given attribute
+      let div = document.querySelector(`[data-col='${colIdx + 1}'][data-row='${rowIdx + 1}']`);
+      //set text content of div to match player turn (X or O)
+      div.innerHTML = SYMBOL[colVal];
+    });
+  });
 }
+
 
 function handleClick (evt) {
   //sets div to the div that tirggered the handleClick event
